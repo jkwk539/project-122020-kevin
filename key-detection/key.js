e7bb63bd8f6de8314e390ea7d90322520fe4bf77
jkwk539 <jkwk539@gmail.com>
2020-12-11 23:20:47 -0500

    working on key detection

---
+++ key-detection/key.js
@@ -0,0 +1,13 @@
+//const pressed = [];
+//const secretCode = 'kevinj';
+
+window.addEventListener('keyup', () => {
+  console.log('keyup');
+});
+
+ // pressed.push(e.key);
+  //pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
+  //if(pressed.join(''))
+  //console.log(pressed)
+
