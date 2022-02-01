6076cc635cc892476167d39a21e86c9168892aea
jkwk539 <jkwk539@gmail.com>
2022-01-31 22:57:04 -0500

    4 files modified

--- home-page/about.js
+++ home-page/about.js
@@ -1,6 +1,16 @@
-$(function(){
+(function(){
   $("#panel").css({"background-color": "yellow", "color": "purple", "display":"none","text-align":"center", 'padding':'20px'});
   $(".about-section").hover(function(){
     $('#panel').slideToggle();
   })
+})
+
+/* Open when someone clicks on the span element */
+(function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+})
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+(function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
 })
